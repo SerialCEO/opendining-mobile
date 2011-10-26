8aeb0cb87517154ed090e6a70323646dde644d3e
Tim Ridgely <tim@opendining.net>
2011-10-26 15:41:11 -0700

    Updated to jQuery Mobile RC2, fixed back button with mobileinit.

--- app/index.php
+++ app/index.php
@@ -5,9 +5,8 @@
 <html> 
 	<head> 
 	<title>Find Food! | Open Dining Network</title> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.css" />
+	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.css" />
 	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.0rc1/jquery.mobile-1.0rc1.min.js"></script>
 
 	<script type="text/javascript">
 		var searchResults;
@@ -86,7 +85,13 @@
 		function stopLoading() {
 			$.mobile.hidePageLoadingMsg();
 		}
+		
+		$(document).bind("mobileinit", function() {
+			$.mobile.page.prototype.options.addBackBtn = true;
+		});
 	</script>
+	
+	<script src="http://code.jquery.com/mobile/1.0rc2/jquery.mobile-1.0rc2.min.js"></script>
 	
 	<style type="text/css">
 	html {
